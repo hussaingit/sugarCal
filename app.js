5d78123045eedc0801cfa839422a31858856a676
hussaingit <newgreatgame514@gmail.com>
2020-06-29 11:15:39 +0500

    sugarCalculation

---
+++ app.js
@@ -0,0 +1,8 @@
+var a = +prompt("enter value")
+var sugarCal = (a*100)/13
+document.write("<b>Calorimeter Result :</b>"+" "+sugarCal+"<br>")
+document.write(" "+"<br>")
+document.write(" "+"<br>")
+document.write(" <b> Thanks for visiting us </b>")
+
+
